827dbd82e5db1837de80ccfeb061ada06dd90f8d
Dougie Richardson <me@dougiewougie.com>
2026-01-01 20:51:29 +0000

    Optimise for mobile

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/images/ProfilePicture.png">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
